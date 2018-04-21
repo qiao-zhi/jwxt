b161120d57ba13059d70c243b04b1c4f5e906d4c
qiaozhi <qiao_liqiang@163.com>
2018-04-21 09:08:57 +0800

    将页面加进去

--- src/main/java/cn/xm/jwxt/utils/DefaultValue.java
+++ src/main/java/cn/xm/jwxt/utils/DefaultValue.java
@@ -10,5 +10,13 @@ public class DefaultValue {
      * 页大小默认值
      */
     public static final int PAGE_SIZE = 8;
+    /**
+     * 代表男性
+     */
+    public static final String SEX_MALE = "1";
+    /**
+     * 代表女性
+     */
+    public static final String SEX_FEMALE = "2";
 
 }
