c9e81341339504d959da3fa6e8c6a6d611ff9527
kanglijun <3287130554@qq.com>
2018-04-23 09:22:28 +0800

    klj

---
+++ src/main/java/cn/xm/jwxt/service/courseDesign/TCoursedesignTeacherArrangeService.java
@@ -0,0 +1,39 @@
+package cn.xm.jwxt.service.courseDesign;
+
+import cn.xm.jwxt.bean.courseDesign.Coursedesignteacherstudent;
+import cn.xm.jwxt.bean.courseDesign.TCoursedesignInfo;
+import cn.xm.jwxt.bean.courseDesign.TCoursedesignTeacherArrange;
+import java.util.List;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+public interface TCoursedesignTeacherArrangeService {
+
+    /**
+     *  在安排课设表中插入信息时需要同时在教师课设安排表批量插入信息
+     * @param list
+     * @return
+     * @throws SQLException
+     */
+    public boolean addTCoursedesignTeacherArrange(List<TCoursedesignTeacherArrange> list) throws SQLException;
+
+    /**
+     *  删除的时候根据安排表中的courseDesignArrangeID删除
+     * @param courseDesignArrangeID
+     * @return
+     * @throws SQLException
+     */
+    public boolean deleteTCoursedesignTeacherArrange(String courseDesignArrangeID) throws SQLException;
+
+    //    public boolean updateTCoursedesignTeacherArrange(TCoursedesignTeacherArrange cdta)throws SQLException;
+
+    /**
+     * 教师根据id,学年等从教师课设安排表开始 查询课设安排
+     * @param condition
+     * @return  可以查出带过的所有课设
+     * @throws SQLException
+     */
+    public List<Map<String,Object>> findCourseDesignerByTeacherId(Map<String, Object> condition) throws SQLException;
+
+}
