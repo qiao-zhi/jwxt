6352fd36b06e96e4fa2c9dd6780ef979d482249b
yuan <guowen.yuan@foxmail.com>
2018-05-07 18:34:48 +0800

    yuan

---
+++ src/main/java/cn/xm/jwxt/controller/orderBooks/TextbookRepositoryController.java
@@ -0,0 +1,30 @@
+package cn.xm.jwxt.controller.orderBooks;
+
+import cn.xm.jwxt.bean.orderBooks.TTextbookBaseInfo;
+import cn.xm.jwxt.controller.arrangeCourse.ApTaskNoticeBaseInfoController;
+import cn.xm.jwxt.service.orderBooks.TextbookRepositoryService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * 项目名称：jwxt
+ * 类描述：教材库管理的Controller
+ * 创建人：yy
+ * 创建时间：2018/5/4 11:52
+ */
+@Controller
+@RequestMapping("/orderBooks")
+public class TextbookRepositoryController {
+    //日志记录器，处理异常
+    private Logger logger = Logger.getLogger(ApTaskNoticeBaseInfoController.class);
+
+    }
+
+
